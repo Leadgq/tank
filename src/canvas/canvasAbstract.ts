f16a4d8debc506b63f217b917d0a3f7144432a25
Leadgq <2643336540@qq.com>
2022-12-11 13:06:37 +0800

    提供位置服务

--- src/canvas/canvasAbstract.ts
+++ src/canvas/canvasAbstract.ts
@@ -1,4 +1,5 @@
 import config from "../config";
+import position from "../service/postion";
 export default abstract class canvasAbstract {
   protected items = [];
   abstract render(): void;
@@ -17,36 +18,9 @@ export default abstract class canvasAbstract {
   }
 
   protected createModels(num: number, model: modelConstructor) {
-    this.positionCollection(num).forEach((position) => {
+    position.getCollection(num).forEach((position) => {
       const instance = new model(this.canvas, position.x, position.y);
       instance.render();
     });
-  }
-  // 生成不重复坐标
-  protected positionCollection(num: number) {
-    let positions = [] as { x: number; y: number }[];
-    for (let i = 0; i < num; i++) {
-      while (true) {
-        const { x, y } = this.position();
-        const exist = positions.some( (position) => position.x === x && position.y === y);
-        if (!exist) {
-          positions.push({ x, y });
-          break;
-        }
-      }
-    }
-    return positions;
-  }
-  // 返回随机坐标
-  protected position() {
-    return {
-      x:
-        Math.floor((Math.random() * config.canvas.width) / config.model.width) *
-        config.model.width,
-      y:
-        Math.floor(
-          (Math.random() * config.canvas.height) / config.model.height
-        ) * config.model.height,
-    };
   }
 }
---
+++ src/service/postion.ts
@@ -0,0 +1,39 @@
+import config from "../config";
+
+type positionType = { x: number; y: number };
+
+class position {
+  collection: positionType[] = [];
+  // 生成不重复坐标
+  public getCollection(num: number) {
+    let positions: positionType[] = [];
+    for (let i = 0; i < num; i++) {
+      while (true) {
+        const { x, y } = this.position();
+        const exist = this.collection.some( (position) => position.x === x && position.y === y);
+        if (!exist) {
+          positions.push({ x, y });
+          this.collection.push({ x, y });
+          break;
+        }
+      }
+    }
+    return positions;
+  }
+  // 返回随机坐标
+  protected position() {
+    return {
+      x:
+        Math.floor((Math.random() * config.canvas.width) / config.model.width) *
+        config.model.width,
+      y:
+        Math.floor(
+          Math.random() * (config.canvas.height / config.model.height - 5)
+        ) *
+          config.model.height +
+        config.model.height * 2,
+    };
+  }
+}
+
+export default new position();
